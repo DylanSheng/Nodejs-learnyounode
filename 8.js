e2158896b1d1fc7f569ea1dd3ad60bf6cb4e62a4
DylanSheng <shengdiansd@gmail.com>
2016-02-26 12:38:11 -0700

    update Q8

---
+++ 8.js
@@ -0,0 +1,15 @@
+var http = require('http')
+var stream = require('stream')
+var bl = require('bl')
+http.get(process.argv[2],function callback(response){
+	response.setEncoding("utf8")
+	response.pipe(bl(function(err,data){
+		data = data.toString()
+		
+		console.log(data.length)
+       		console.log(data)
+		})
+	)
+		
+}
+)
