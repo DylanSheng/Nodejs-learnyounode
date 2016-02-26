d084a54670ca72982ff8c461df5a6913c81ec8b0
DylanSheng <shengdiansd@gmail.com>
2016-02-26 15:33:39 -0700

    update Q12

---
+++ 12.js
@@ -0,0 +1,8 @@
+var map = require('through2-map')  
+var http = require('http')
+var server = http.createServer(function(req,res){
+	req.pipe(map(function (chunk) {  
+	   return chunk.toString().split('').join('').toUpperCase()  
+	})).pipe(res)  
+})
+server.listen(process.argv[2])
